db248f80f953352c7e433a6627395f992d2e360f
Lourdes <lopezlourdes017@gmail.com>
2026-06-30 00:35:45 -0300

    cambios entidades faltantes y otras correcciones

---
+++ src/main/java/com/subastas/entity/Producto.java
@@ -0,0 +1,40 @@
+package com.subastas.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "productos")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Producto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String titulo;
+
+    @Column(length = 1000)
+    private String descripcion;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "categoria_id")
+    private Categoria categoria;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "vendedor_id")
+    private Usuario vendedor;
+
+    @Builder.Default
+    private boolean eliminado = false;
+
+    @Builder.Default
+    private boolean moderado = false;
+
+
+}
