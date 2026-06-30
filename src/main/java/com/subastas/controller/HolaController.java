10bcdc64a382071a218459cb4fdd07c74fae9bcc
Emilia <m.emilia46villarruel@gmail.com>
2026-06-30 08:16:42 -0300

    Redirecciona la página principal al frontend

--- src/main/java/com/subastas/controller/HolaController.java
+++ src/main/java/com/subastas/controller/HolaController.java
@@ -1,15 +1,13 @@
 package com.subastas.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HolaController {
 
     @GetMapping("/")
-    @ResponseBody
-    public String hola() {
-        return "Bienvenido al sistema de subastas online";
+    public String inicio() {
+        return "redirect:/index.html";
     }
 }
